fe90189368ac0506d789bc22ad5d6f0d4046d1d6
Foysal Ahmed <foysal.ahmed@reddotdigitalit.com>
2024-10-06 01:02:56 +0600

    Initial commit of movie-list-app

---
+++ src/main/java/com/example/movie_list_app/MovieListAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.movie_list_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieListAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieListAppApplication.class, args);
+	}
+
+}
